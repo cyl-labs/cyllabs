ef64f021ec16512c474ae4ac685db90070498e58
kohct1 <kohct2006@gmail.com>
2025-08-13 01:05:33 +0800

    fix: fix calculator missing animation

--- src/app/components/calculator/CalculatorReach.tsx
+++ src/app/components/calculator/CalculatorReach.tsx
@@ -22,13 +22,34 @@ export default function CalculatorReach({
   return (
     <div className="h-full max-h-[500px] h-full max-h-[500px] flex flex-col flex-grow justify-between py-8">
       <div className="flex flex-col gap-8">
-        <h1 className="text-[64px] text-white font-semibold">
+        <motion.h1
+          className="text-[64px] text-white font-semibold"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 30,
+            restDelta: 0.001,
+          }}
+        >
           How Many People See You Each Month?
-        </h1>
-        <p className="text-[20px] text-[#999999] leading-[1.2] tracking-normal">
+        </motion.h1>
+        <motion.p
+          className="text-[20px] text-[#999999] leading-[1.2] tracking-normal"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 30,
+            restDelta: 0.001,
+            delay: 0.2,
+          }}
+        >
           Give us your best guess. We’ll turn it into the number of leads and
           sales you could be getting.
-        </p>
+        </motion.p>
       </div>
       <motion.div
         className="flex flex-col gap-4"
