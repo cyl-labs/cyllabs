a20fd3e8d1efeff163129026db5ba713097fbec7
kohct1 <kohct2006@gmail.com>
2025-08-18 10:52:55 +0800

    feat: add whatsapp icons

--- src/app/components/home/Hero.tsx
+++ src/app/components/home/Hero.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { motion } from "framer-motion";
 import Navbar from "../Navbar";
 import Wrapper from "../Wrapper";
+import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
@@ -30,8 +32,8 @@ export default function Hero() {
             </h1>
             <div className="w-8 h-8 bg-[#FD5001] rounded-full mb-10 max-[1200px]:w-[22px] max-[1200px]:h-[22px] max-[1200px]:mb-6 max-md:w-4 max-md:h-4 max-md:mb-5 max-sm:w-[10px] max-sm:h-[10px] max-sm:mb-3"></div>
           </motion.div>
-          <motion.p
-            className="text-[28px] opacity-70"
+          <motion.div
+          className="flex justify-end items-center flex-wrap gap-8"
             initial={{ x: 100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{
@@ -42,8 +44,37 @@ export default function Hero() {
               delay: 0.2,
             }}
           >
-            cut your losses.
-          </motion.p>
+            <p className="text-[28px] opacity-70">cut your losses.</p>
+            <Button
+              asChild
+              className="w-fit h-fit max-sm:self-center bg-[#FD5001] rounded-full !px-8 !py-4 text-[20px] text-white font-semibold overflow-hidden relative group"
+            >
+              <Link
+                href="https://wa.me/6581977376?text=Hi%2C%20I%20would%20like%20to%20claim%20my%20free%20consultation"
+                target="_blank"
+              >
+                <motion.div className="flex items-center gap-2 max-sm:justify-center">
+                  <span>Contact us</span>
+                  <motion.svg
+                    className="min-w-6 min-h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <motion.path
+                      d="M7 7H17M17 7V17M17 7L7 17"
+                      stroke="white"
+                      strokeWidth="2.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </motion.svg>
+                </motion.div>
+              </Link>
+            </Button>
+          </motion.div>
         </div>
       </Wrapper>
     </div>
