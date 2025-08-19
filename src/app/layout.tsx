498bbe29da72e128dbff270a55899fda7e2e5826
kohct1 <kohct2006@gmail.com>
2025-08-19 14:23:14 +0800

    feat: update meta pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1147543727241214');
+            fbq('init', '744216805338259');
             fbq('track', 'PageView');
           `}
         </Script>
